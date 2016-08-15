f0a35b933605ae6918cebe55be9e0bacad16a17b
Justin Boltz <boltz.justin@gmail.com>
2016-08-15 13:28:43 -0500

    Encapsulate item expiry

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
+  LIFETIME_IN_SECS = 7.days
+
+  def self.delete_expired
+    where(
+      "created_at <= ?",
+      Time.now - LIFETIME_IN_SECS
+    ).destroy_all
+  end
 end
--- lib/tasks/todo.rake
+++ lib/tasks/todo.rake
@@ -1,7 +1,7 @@
 namespace :todo do
   desc "deletes items that hit 7 day expiration"
   task delete_items: :environment do
-    Item.where("created_at <= ?", Time.now - 7.days).destroy_all
+    Item.delete_expired
   end
 
 end
